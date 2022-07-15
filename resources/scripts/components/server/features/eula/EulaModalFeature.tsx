cea0f03338fee060140cbe27782ab3350f89e3cf
ilworkcn <1061372739@qq.com>
2022-07-15 14:46:06 +0800

    更新features

--- resources/scripts/components/server/features/eula/EulaModalFeature.tsx
+++ resources/scripts/components/server/features/eula/EulaModalFeature.tsx
@@ -1,3 +1,11 @@
+/*
+ * Pterodactyl CHINA - Panel | Jexactyl Branch
+ * Simplified Chinese Translation Copyright (c) 2021 - 2022 Ice Ling <iceling@ilwork.cn>
+ * Please note the attribution when cite
+ * This software is licensed under the terms of the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+
 import tw from 'twin.macro';
 import useFlash from '@/plugins/useFlash';
 import { ServerContext } from '@/state/server';
@@ -65,16 +73,16 @@ const EulaModalFeature = () => {
             showSpinnerOverlay={loading}
         >
             <FlashMessageRender key={'feature:eula'} css={tw`mb-4`} />
-            <h2 css={tw`text-2xl mb-4 text-neutral-100`}>Accept Minecraft&reg; EULA</h2>
+            <h2 css={tw`text-2xl mb-4 text-neutral-100`}>同意 Minecraft (我的世界)&reg; 最终用户许可协议</h2>
             <p css={tw`text-neutral-200`}>
-                By pressing {'"I Accept"'} below you are indicating your agreement to the&nbsp;
+                请点击 {'"我接受"'} 来同意&nbsp;
                 <a
                     target={'_blank'}
                     css={tw`text-primary-300 underline transition-colors duration-150 hover:text-primary-400`}
                     rel={'noreferrer noopener'}
                     href='https://account.mojang.com/documents/minecraft_eula'
                 >
-                    Minecraft&reg; EULA
+                    Minecraft (我的世界)&reg; 最终用户许可协议
                 </a>
                 .
             </p>
@@ -84,10 +92,10 @@ const EulaModalFeature = () => {
                     onClick={() => setVisible(false)}
                     css={tw`w-full sm:w-auto border-transparent`}
                 >
-                    Cancel
+                    不接受
                 </Button>
                 <Button onClick={onAcceptEULA} css={tw`mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto`}>
-                    I Accept
+                    我接受
                 </Button>
             </div>
         </Modal>
